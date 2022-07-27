937e462511cd3cda209cbe11f6a7082a9742d1ef
Ahmed Ashraf Mahmoud <109146739+0AhmedAshraf@users.noreply.github.com>
2022-07-27 19:30:08 +0200

    Add files via upload

---
+++ lec_5/assigment2/src/assigment2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int factorial(int n);
+int main(){
+	int n;
+	printf("Enter an positive integer:");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&n);
+	printf("Factorial of %d = %d",n,factorial(n));
+
+
+}
+int factorial(int n)
+{
+	if(n!=1)
+	{
+		return n*factorial(n-1);
+	}
+}
---
+++ lec_5/assigment3/src/assigment3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void rev();
+int main() {
+	printf("Enter a sentence: ");
+	fflush(stdin);fflush(stdout);
+	rev();
+	return 0;
+
+}
+void rev()
+{
+	char c;
+	scanf("%c",&c);
+	if(c!='\n')
+	{
+		rev();
+		printf("%c",c);
+	}
+}
